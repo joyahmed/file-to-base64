2c00b080aff9986d8a3632eed0c7725be90fb6ba
joyahmed <joy_ahmed_007@yahoo.com>
2022-05-27 10:43:31 +0600

    first commit without much style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Image = () => {
+	const [files, setFiles] = useState([]);
+	const [copied, setCopied] = useState(false);
 
-export default App;
+	const handleChange = e => {
+		let files = e.target.files;
+
+		let allFiles = [];
+		for (let i = 0; i < files.length; i++) {
+			let file = files[i];
+
+			let reader = new FileReader();
+			reader.readAsDataURL(file);
+
+			reader.onload = () => {
+				let fileInfo = {
+					name: file.name,
+					url: reader.result
+				};
+
+				allFiles.push(fileInfo);
+				setFiles(allFiles);
+			};
+		}
+	};
+
+	const clearImages = () => {
+		console.log('Button clicked');
+		setFiles(null);
+	};
+	return (
+		<div className='flex flex-col items-center justify-center mt-10 w-full px-5'>
+			<div
+				className={`flex flex-row items-center ${
+					files && files.length > 0
+						? 'justify-between px-5 transittion-all duration-300 ease origin-center'
+						: 'justify-center transittion-all duration-300 ease'
+				} text-center w-full `}>
+				<div>
+					<input type='file' onChange={handleChange} multiple />
+				</div>
+				{files && files.length > 0 && (
+					<div>
+						<Button onClick={() => clearImages()} text='Clear Images' />
+					</div>
+				)}
+			</div>
+
+			<div className='grid grid-cols-2 w-full mx-2'>
+				{files &&
+					files.length > 0 &&
+					files.map((file, index) => (
+						<div
+							key={index}
+							className='justify-between my-5 text-xs text-white font-semibold px-5  overflow-hidden'>
+							<div className='flex bg-gray-500 px-5 py-5 rounded-md shadow-md'>
+								<img className='w-28 h-28 rounded-md' src={file.url} alt='' />
+								<div className='flex flex-col justify-between px-5 py-1'>
+									<div className='flex'>Name: {file.name}</div>
+									<div className=''>Url: {file.url.substr(0, 40)}</div>
+									<div className=''>
+										<CopyToClipboard
+											text={file.url}
+											onCopy={() => setCopied(!copied)}>
+											<Button text='Copy URL' />
+										</CopyToClipboard>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+			</div>
+		</div>
+	);
+};
+
+export default Image;
+
+const Button = ({ text, onClick }) => {
+	return (
+		<button
+			onClick={onClick}
+			className='bg-gray-400 px-2 py-1 rounded-md font-semibold'>
+			{text}
+		</button>
+	);
+};
